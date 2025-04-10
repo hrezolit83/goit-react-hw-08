ea204a7fea597578aae1183376db314dcdb72144
hrezolit83 <deduh83@gmail.com>
2025-04-11 00:28:11 +0300

    add

--- src/components/registrationForm/RegistrationForm.jsx
+++ src/components/registrationForm/RegistrationForm.jsx
@@ -2,39 +2,46 @@ import { useDispatch } from "react-redux";
 import { Formik, Form, Field } from "formik";
 import { register } from "../../redux/auth/operations";
 import css from "./RegistrationForm.module.css";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function RegistrationForm() {
   const dispatch = useDispatch();
 
   const handleSubmit = (values, actions) => {
-    dispatch(register(values));
+    dispatch(register(values))
+      .unwrap()
+      .then(() => toast.success("User successfully registered!"))
+      .catch(() => toast.error("Error! Uncorrect data."));
     actions.resetForm();
   };
 
   return (
-    <Formik
-      initialValues={{
-        name: "",
-        email: "",
-        password: "",
-      }}
-      onSubmit={handleSubmit}
-    >
-      <Form className={css.form} autoComplete="off">
-        <label className={css.label}>
-          Username
-          <Field type="text" name="name" />
-        </label>
-        <label className={css.label}>
-          Email
-          <Field type="email" name="email" />
-        </label>
-        <label className={css.label}>
-          Password
-          <Field type="password" name="password" />
-        </label>
-        <button type="submit">Register</button>
-      </Form>
-    </Formik>
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          password: "",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <Form className={css.form} autoComplete="off">
+          <label className={css.label}>
+            Username
+            <Field type="text" name="name" />
+          </label>
+          <label className={css.label}>
+            Email
+            <Field type="email" name="email" />
+          </label>
+          <label className={css.label}>
+            Password
+            <Field type="password" name="password" />
+          </label>
+          <button type="submit">Register</button>
+        </Form>
+      </Formik>
+    </>
   );
 }
